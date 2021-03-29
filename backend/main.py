a5f929e340c0afe44c73e048dc4f50e5e0bbb2c4
atulwadekar <71044114+atulwadekar@users.noreply.github.com>
2021-03-29 17:37:55 -0400

    Add files via upload

---
+++ backend/main.py
@@ -0,0 +1,31 @@
+from flask import Flask
+import json
+from flask_cors import CORS
+from datetime import datetime
+
+# --------- API CODE --------- 
+api = Flask(__name__)
+CORS(api)
+
+# GET all the data from the file
+@api.route('/test', methods=['GET'])
+def get_companies():
+  return "The /test endpoint is working!"
+
+@api.route('/api/loc/<location>', methods=['GET'])
+def getlocation(location):
+  return "yourCodeHere"
+
+# Date expected in YYYY-DD-MM format
+@api.route('/api/date/<date>/loc/<location>', methods=['GET'])
+def getdate(_date, location):
+  return "yourCodeHere"
+
+# Expected format for dates are YYYY-DD-MM
+@api.route('/api/date/<_dateFrom>/<_dateTo>/loc/<location>', methods=['GET'])
+def getdaterange(_dateFrom, _dateTo, location):
+  return "yourCodeHere"
+
+if __name__ == '__main__':
+    api.run()
+
